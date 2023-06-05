8b9c5c82f7150a2430d3a0a6f58208240461e5a2
israel_manzi <104481099+israelmanzi@users.noreply.github.com>
2023-06-05 08:38:30 +0200

    [ft]: product purchase

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require 'vendor/autoload.php';
 
+use Models\Product;
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -24,10 +25,10 @@ $app->post('/product/create', function (Request $request, Response $response, $a
     $data = json_decode($request->getBody(), true);
 
     $v = new Validator($data);
-    $v->rule('required', ['name', 'price', 'description', 'category', 'tax', 'amount']);
-    $v->rule('numeric', ['tax', 'amount', 'price']);
-    $v->rule('min', 'amount', 1);
-    $v->rule('min', 'price', 0.01);
+    $v->rule('required', ['name', 'amount', 'description', 'category', 'tax', 'quantity']);
+    $v->rule('numeric', ['tax', 'quantity', 'amount']);
+    $v->rule('min', 'quantity', 1);
+    $v->rule('min', 'amount', 0.01);
     $v->rule('min', 'tax', 0.01);
     $v->rule('max', 'tax', 100);
 
@@ -38,7 +39,7 @@ $app->post('/product/create', function (Request $request, Response $response, $a
 
     $data['tax'] = $data['tax'] / 100;
 
-    $product = new Models\Product($data['name'], $data['price'], $data['description'], $data['category'], $data['tax'], $data['amount']);
+    $product = new Models\Product($data['name'], $data['amount'], $data['description'], $data['category'], $data['tax'], $data['quantity']);
 
     $new_prd = $product->save();
 
@@ -89,6 +90,101 @@ $app->get('/product/{id}', function (Request $request, Response $response, $args
     return $response->withHeader('Content-Type', 'application/json');
 });
 
+$app->delete('/product/{id}', function (Request $request, Response $response, $args) {
+    if (!Util::isUUID($args['id'])) {
+        $response = $response->withStatus(400);
+        $response->getBody()->write(json_encode(['message' => 'Invalid product id!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $product = Models\Product::find($args['id']);
+
+    if (!$product) {
+        $response = $response->withStatus(404);
+        $response->getBody()->write(json_encode(['message' => 'Product with id #' . $args['id'] . ' not found!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $res = Models\Product::delete($args['id']);
+
+    if (!$res) {
+        $response = $response->withStatus(500);
+        $response->getBody()->write(json_encode(['message' => 'Some thing went wrong on the server!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $response = $response->withStatus(200);
+    $response->getBody()->write(json_encode(['message' => 'Product with id #' . $args['id'] . ' deleted successfully!']));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->patch('/product/{id}', function (Request $request, Response $response, $args) {
+    if (!Util::isUUID($args['id'])) {
+        $response = $response->withStatus(400);
+        $response->getBody()->write(json_encode(['message' => 'Invalid product id!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $product = Models\Product::find($args['id']);
+
+    if (!$product) {
+        $response = $response->withStatus(404);
+        $response->getBody()->write(json_encode(['message' => 'Product with id #' . $args['id'] . ' not found!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $data = json_decode($request->getBody(), true);
+
+    $v = new Validator($data);
+    $v->rule('required', ['name', 'quantity', 'description', 'category', 'tax', 'amount']);
+    $v->rule('numeric', ['tax', 'amount', 'quantity']);
+    $v->rule('min', 'quantity', 1);
+    $v->rule('min', 'amount', 0.01);
+    $v->rule('min', 'tax', 0.01);
+    $v->rule('max', 'tax', 100);
+
+    if (!$v->validate()) {
+        $response->getBody()->write(json_encode(['message' => $v->errors()]));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $data['tax'] = $data['tax'] / 100;
+
+    $product['name'] = $data['name'];
+    $product['quantity'] = $data['quantity'];
+    $product['description'] = $data['description'];
+    $product['category'] = $data['category'];
+    $product['tax'] = $data['tax'];
+    $product['amount'] = $data['amount'];
+
+    $res = Models\Product::update($product, $args['id']);
+
+    if (!$res) {
+        $response = $response->withStatus(500);
+        $response->getBody()->write(json_encode(['message' => 'Some thing went wrong on the server!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $response = $response->withStatus(200);
+    $response->getBody()->write(json_encode(['message' => 'Product with id #' . $args['id'] . ' updated successfully!']));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+// $app->get('/product/search/{query}', function (Request $request, Response $response, $args) {
+//     $products = Models\Product::search($args['query']);
+
+//     if (!$products) {
+//         $response = $response->withStatus(404);
+//         $response->getBody()->write(json_encode(['message' => 'No products found!']));
+//         return $response->withHeader('Content-Type', 'application/json');
+//     }
+
+//     $response = $response->withStatus(200);
+//     $response->getBody()->write(json_encode(['products' => $products]));
+//     return $response->withHeader('Content-Type', 'application/json');
+// });
+
 $app->get('/product/category/{category_id}', function (Request $request, Response $response, $args) {
     if (!Util::isUUID($args['category_id'])) {
         $response = $response->withStatus(400);
@@ -103,7 +199,7 @@ $app->get('/product/category/{category_id}', function (Request $request, Respons
         $response->getBody()->write(json_encode(['message' => 'Category with id #' . $args['category_id'] . ' not found!']));
         return $response->withHeader('Content-Type', 'application/json');
     }
-    
+
     $products = Models\Product::findByCategory($args['category_id']);
 
     if (!$products) {
@@ -152,12 +248,113 @@ $app->post('/category/create', function (Request $request, Response $response, $
 
     if (!$new_cat) {
         $response = $response->withStatus(500);
-        $response->getBody()->write(json_encode(['message' => 'Some thing is wrong on the server!']));
+        $response->getBody()->write(json_encode(['message' => 'Some thing went wrong on the server!']));
         return $response->withHeader('Content-Type', 'application/json');
     }
 
     $response = $response->withStatus(201);
     $response->getBody()->write(json_encode(['message' => 'Category created successfully!']));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->post('/product/purchase', function (Request $request, Response $response, $args) {
+    $data = json_decode($request->getBody(), true);
+
+    $v = new Validator($data);
+
+    $v->rule('required', ['product_id', 'amount']);
+    $v->rule('integer', 'amount');
+
+    if (!$v->validate()) {
+        $response = $response->withStatus(400);
+        $response->getBody()->write(json_encode(['message' => $v->errors()]));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    if (!Util::isUUID($data['product_id'])) {
+        $response = $response->withStatus(400);
+        $response->getBody()->write(json_encode(['message' => 'Invalid product id!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $product = Models\Product::find($data['product_id']);
+
+    if (!$product) {
+        $response = $response->withStatus(404);
+        $response->getBody()->write(json_encode(['message' => 'Product with id #' . $data['product_id'] . ' not found!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $purchase = new Models\Purchase($data['amount']);
+
+    $new_purchase = $purchase->save($data['product_id']);
+
+    if (!$new_purchase) {
+        $response = $response->withStatus(500);
+        $response->getBody()->write(json_encode(['message' => 'Some thing went wrong on the server! here']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $res = Models\Product::updateStock($data['product_id']);
+
+    if (!$res) {
+        $response = $response->withStatus(500);
+        $response->getBody()->write(json_encode(['message' => 'Some thing went wrong on the server! 2']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $response = $response->withStatus(201);
+
+    $response->getBody()->write(json_encode(['message' => 'Purchase created successfully!']));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/product/purchase/{id}', function (Request $request, Response $response, $args) {
+    if (!Util::isUUID($args['id'])) {
+        $response = $response->withStatus(400);
+        $response->getBody()->write(json_encode(['message' => 'Invalid purchase id!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $purchase = Models\Purchase::find($args['id']);
+
+    if (!$purchase) {
+        $response = $response->withStatus(404);
+        $response->getBody()->write(json_encode(['message' => 'Purchase with id #' . $args['id'] . ' not found!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $response = $response->withStatus(200);
+    $response->getBody()->write(json_encode(['purchase' => $purchase]));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/purchase/list-all', function (Request $request, Response $response, $args) {
+    $purchases = Models\Purchase::all();
+
+    if (!$purchases) {
+        $response = $response->withStatus(404);
+        $response->getBody()->write(json_encode(['message' => 'No purchases found!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $response = $response->withStatus(200);
+    $response->getBody()->write(json_encode(['purchases' => $purchases]));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+#get recently purchased products
+$app->get('/purchase/list-recent', function (Request $request, Response $response, $args) {
+    $purchases = Models\Purchase::recent();
+
+    if (!$purchases) {
+        $response = $response->withStatus(404);
+        $response->getBody()->write(json_encode(['message' => 'No purchases found!']));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $response = $response->withStatus(200);
+    $response->getBody()->write(json_encode(['purchases' => $purchases]));
     return $response->withHeader('Content-Type', 'application/json');
 });
 
